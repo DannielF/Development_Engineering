fc550c1ee37b4f315da0165a17cbc530573f0a4b
DannielF <danielgranados1992@gmail.com>
2020-12-25 22:18:46 -0500

    Add EcmaScript-7

---
+++ EcmaScript/src/es7/index.js
@@ -0,0 +1,18 @@
+// include trabaja sobre un arreglo o string
+// nos permite sabes si hay un elemento dentro de este valor
+
+let numbers = [1, 2, 4, 7, 8];
+
+if (numbers.includes(7)) {
+  console.log('we found number 7 in the array');
+} else {
+  console.log('we can´t find anything');
+}
+
+// elevar a la potencia
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
