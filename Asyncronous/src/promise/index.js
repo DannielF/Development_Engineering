3afb9005c2c4b83f8b5005308e1d21cc23060ab2
DannielF <danielgranados1992@gmail.com>
2021-01-05 19:10:55 -0500

    Add Promise and update Package.JSON

---
+++ Asyncronous/src/promise/index.js
@@ -0,0 +1,44 @@
+// promise
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve('Hey!');
+    } else {
+      reject('oops!');
+    }
+  });
+};
+
+somethingWillHappen()
+  .then(Response => console.log(Response))
+  .catch(err => console.error(err));
+
+// promise with error
+// it's important to used new error
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve('True');
+      }, 2000)
+    } else {
+      const error = new Error('Oops');
+      reject(error);
+    }
+  });
+}
+
+somethingWillHappen2()
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+// run all promises
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then(response => {
+    console.log('Array of results', response);
+  })
+  .catch(err => {
+    console.error(err);
+  });
