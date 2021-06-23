9ebd0d1088f947d3a3fce3a8c431ff821840c986
dannielf <danielgranados1992@gmail.com>
2021-06-23 13:06:47 -0500

    POO intro

---
+++ POOUber/Java/main/main.java
@@ -0,0 +1,9 @@
+class main{
+  public static void main(string[] args) {
+    Car car = new Car();
+    car.license = "HGB243";
+    car.driver = "Juanito Escarcha";
+    car.passenger = 4;
+    System.out.println("Car license: " + car.license);
+  }
+}
---
+++ POOUber/Python/main.py
@@ -0,0 +1,7 @@
+from car import Car
+
+if __name__ == "__main__":
+    car = Car()
+    car.license = 'HPI666'
+    car.driver = 'Benito Camelas'
+    print(vars(car))
