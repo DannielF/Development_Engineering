85eefcd0cbc34d696ce1a468455e3f387b4f4d27
DannielF <danielgranados1992@.gmail.com>
2021-07-17 15:12:02 -0500

    Add container components, create first component

---
+++ reactJS/src/components/Header.jsx
@@ -0,0 +1,26 @@
+// Stateless component
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <div className='header__logo'>
+      <img
+        className='header__img'
+        src='./PlatziVideo/logo-platzi-video-BW2.png'
+        alt='Logo'
+      />
+    </div>
+    <div className='header__menu'>
+      <div className='header__menu--profile'>
+        <img src='./PlatziVideo/user-icon.png' alt='User icon' />
+        <p>Perfil</p>
+      </div>
+      <ul>
+        <li><a className='header__menu--links' href='/login.html'>Cuenta</a></li>
+        <li><a className='header__menu--links' href='/'>Cerrar sesión</a></li>
+      </ul>
+    </div>
+  </header>
+);
+
+export default Header;
