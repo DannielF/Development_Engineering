7b911ecce25bd8acede2b5cc8a5d1e942c67434f
DannielF <danielgranados1992@.gmail.com>
2021-07-20 15:19:55 -0500

    Custom hooks, modify CarouselItem

--- reactJS/src/components/CarouselItem.jsx
+++ reactJS/src/components/CarouselItem.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import '../assets/styles/components/Carousel.scss';
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
   <div className='carousel-item'>
     <img
       className='carousel-item__img'
-      src='./Photos/photo1.jpg'
-      alt='groups'
+      src={cover}
+      alt={title}
     />
     <div className='carousel-item__details'>
       <div>
         <img src='./PlatziVideo/icons8-play-64.png' alt='Play icon' />
         <img src='./PlatziVideo/icons8-plus-64.png' alt='Plus icon' />
       </div>
-      <p className='carousel-item__details--title'>Titulo descriptivo</p>
-      <p className='carousel-item__details--subtitle'>2020 16+ 114 minutos</p>
+      <p className='carousel-item__details--title'>{title}</p>
+      <p className='carousel-item__details--subtitle'>{` ${year} ${contentRating} ${duration} `}</p>
     </div>
   </div>
 );
--- reactJS/src/containers/App.jsx
+++ reactJS/src/containers/App.jsx
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
+const API = 'API';
+
 const App = () => {
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-    fetch(API)
-      .then(response => response.json())
-      .then(data => setVideos(data));
-  }, []);
-
   return (
     <div className='App'>
       <Header />
       <Search />
 
-      <Categories title='Your list'>
+      {useInitialState.mylist.length > 0 && (
+        <Categories title='Your list'>
+          <Carousel>
+            {useInitialState.mylist.map((item) => <CarouselItem key={item.id} {...item} />)}
+          </Carousel>
+        </Categories>
+      )}
+
+      <Categories title='Trends'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {useInitialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
 
