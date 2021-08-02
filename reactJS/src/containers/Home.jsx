bc662b227cf2f12a08e4886d09ba2d802ef79a37
DannielF <danielgranados1992@.gmail.com>
2021-08-02 16:49:36 -0500

    Create login form

--- reactJS/src/containers/Home.jsx
+++ reactJS/src/containers/Home.jsx
@@ -1,3 +1,5 @@
+/* eslint arrow-parens: ["error", "as-needed"] */
+/* eslint-disable react/jsx-wrap-multilines */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { connect } from 'react-redux';
@@ -15,20 +17,20 @@ const Home = ({ myList, trends, originals }) => {
       {myList.length > 0 &&
         <Categories title='Your list'>
           <Carousel>
-            {myList.map((item) => <CarouselItem key={item.id} {...item} isList />)}
+            {myList.map(item => <CarouselItem key={item.id} {...item} isList />)}
           </Carousel>
         </Categories>
       }
 
       <Categories title='Trends'>
         <Carousel>
-          {trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+          {trends.map(item => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
 
       <Categories title='Originals'>
         <Carousel>
-          {originals.map((item) => <CarouselItem key={item.id} {...item} />)}
+          {originals.map(item => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
 
--- reactJS/src/containers/Login.jsx
+++ reactJS/src/containers/Login.jsx
@@ -1,56 +1,76 @@
-import React from 'react';
+/* eslint-disable arrow-parens */
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Login.scss';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
-const Login = () => (
-  <section className='login'>
-    <section className='login__container'>
-      <h2>Inicia sesión</h2>
-      <form className='login__container--form'>
-        <input
-          aria-label='correo'
-          className='input'
-          type='text'
-          placeholder='Correo'
-        />
-        <input
-          aria-label='contraseña'
-          className='input'
-          type='password'
-          placeholder='Contraseña'
-        />
-        <button type='button' className='button'>Iniciar sesión</button>
-        <div className='login__container--remember'>
-          <label htmlFor='cbox1'>
-            <input type='checkbox' name='' id='cbox1' value='checkbox' />
-            Recuérdame
-          </label>
-          <a href='/'>Olvidé mi contraseña</a>
-        </div>
-      </form>
-      <section className='login__container--social-media'>
-        <div>
-          <img
-            src={googleIcon}
-            alt='logo google'
+const Login = () => {
+  const [form, setValues] = useState({
+    email: '',
+  });
+  const handleInput = event => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(form);
+  };
+  return (
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Log In</h2>
+        <form className='login__container--form' onSubmit={handleSubmit}>
+          <input
+            aria-label='Mail'
+            name='Email'
+            className='input'
+            type='text'
+            placeholder='email'
+            onChange={handleInput}
           />
-          <p>Inicia sesion con Google</p>
-        </div>
-        <div>
-          <img src={twitterIcon} alt='logo Twitter' />
-          <p>Inicia sesion con Twitter</p>
-        </div>
+          <input
+            aria-label='password'
+            name='Password'
+            className='input'
+            type='password'
+            placeholder='Password'
+            onChange={handleInput}
+          />
+          <button type='button' className='button'>Log In</button>
+          <div className='login__container--remember'>
+            <label htmlFor='cbox1'>
+              <input type='checkbox' name='' id='cbox1' value='checkbox' />
+              Remember me
+            </label>
+            <a href='/'>I forgot my password</a>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div>
+            <img
+              src={googleIcon}
+              alt='logo google'
+            />
+            <p>Sign in with Google</p>
+          </div>
+          <div>
+            <img src={twitterIcon} alt='logo Twitter' />
+            <p>Sign in with Twitter</p>
+          </div>
+        </section>
+        <p className='login__container--register'>
+          You don&apos;t have account
+          <Link to='/register'>
+            Sign In
+          </Link>
+        </p>
       </section>
-      <p className='login__container--register'>
-        No tienes ninguna cuenta
-        <Link to='/register'>
-          Regístrate
-        </Link>
-      </p>
     </section>
-  </section>
-);
+  );
+};
 
 export default Login;
