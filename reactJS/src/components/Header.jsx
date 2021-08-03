744b80991c2f9c882fd3b4f87c7a34bf41f40c80
DannielF <danielgranados1992@.gmail.com>
2021-08-03 16:34:23 -0500

    Update Register component

--- reactJS/src/components/Header.jsx
+++ reactJS/src/components/Header.jsx
@@ -1,14 +1,23 @@
+/* eslint-disable arrow-parens */
+/* eslint-disable react/jsx-wrap-multilines */
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logoPlatzi from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
 const Header = (props) => {
   const { user } = props;
-  const hasUser = Object.keys(user).lenght > 0;
+  const hasUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
+
   return (
     <header className='header'>
       <div className='header__logo'>
@@ -28,21 +37,29 @@ const Header = (props) => {
           <p>Profile</p>
         </div>
         <ul>
-          <li>
-            <a className='header__menu--links' href='/login.html'>
-              Account
-            </a>
-          </li>
-          <li>
-            <Link to='/login' className='header__menu--links'>
-              Log In
-            </Link>
-          </li>
+          {hasUser ?
+            <li>
+              <a className='header__menu--links' href='/login.html'>
+                Account
+              </a>
+            </li> : null}
+          {hasUser ?
+            <li><a href='#Logout' onClick={handleLogout}>Logout</a></li> :
+            <li>
+              <Link to='/login' className='header__menu--links'>
+                Log In
+              </Link>
+            </li>}
         </ul>
       </div>
     </header>
   );
-}
+};
+
+Header.propTypes = {
+  user: PropTypes.object,
+  logoutRequest: PropTypes.func,
+};
 
 const mapStateToProps = state => {
   return {
@@ -50,4 +67,8 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- reactJS/src/containers/Register.jsx
+++ reactJS/src/containers/Register.jsx
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Register.scss';
 
-const Register = () => (
-  <section className='register'>
-    <section className='register__container'>
-      <h2>Regístrate</h2>
-      <form className='register__container--form'>
-        <input className='input' type='text' placeholder='Nombre' />
-        <input className='input' type='email' placeholder='Correo' />
-        <input className='input' type='password' placeholder='Contraseña' />
-        <button type='button' className='button'>Registrarme</button>
-      </form>
-      <Link to='/login' className='register__container--login'>
-        Iniciar sesión
-      </Link>
-    </section>
-  </section>
-);
+const Register = () => {
+  const [form, setValues] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
 
+  const handleInput = event => {
+    setValues({
+      ...form,
+      [event.target.name] = event.target.value,
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(form);
+  }
+
+  return (
+    <section className='register'>
+      <section className='register__container'>
+        <h2>Create account</h2>
+        <form className='register__container--form' onSubmit={handleSubmit} >
+          <input
+            name='name'
+            className='input'
+            type='text'
+            placeholder='Name'
+            onChange={handleInput}
+          />
+          <input
+            name='email'
+            className='input'
+            type='email'
+            placeholder='Email'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            className='input'
+            type='password'
+            placeholder='Password'
+            onChange={handleInput}
+          />
+          <button type='submit' className='button'>Sign In</button>
+        </form>
+        <Link to='/login' className='register__container--login'>
+          Log In
+        </Link>
+      </section>
+    </section>
+  );
+};
 export default Register;
