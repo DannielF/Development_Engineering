16c58c37725ae30769efcddc1989f2b5295ca216
DannielF <danielgranados1992@.gmail.com>
2021-10-09 16:22:33 -0500

    ReactPratico mk component, pages, routes, styles

---
+++ reactPratico/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <img src="https://unsplash.com/photos/By-tZImt0Ms" alt="Error 404" />
+    </div>
+  )
+}
+
+export default NotFound
