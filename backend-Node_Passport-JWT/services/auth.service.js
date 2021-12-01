c23b839d8cd32bbbd272992b5590bae1eda81112
danielgranados1992@gmail.com <danielgranados1992@gmail.com>
2021-12-01 17:03:34 -0500

    backend-node JWT nodemailer

---
+++ backend-Node_Passport-JWT/services/auth.service.js
@@ -0,0 +1,78 @@
+const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+
+const { config } = require('../config/config');
+const UserService = require('./user.service');
+const service = new UserService();
+
+class AuthService {
+
+  async getUSer(email, password) {
+    const user = await service.findEmail(email);
+    if (!user) {
+      throw (boom.unauthorized());
+    }
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      throw (boom.unauthorized());
+    }
+    // user is returned by sequelize
+    delete user.dataValues.password;
+    return user;
+  }
+
+  signToken(user) {
+    const payload = {
+      sub: user.id,
+      role: user.role,
+    };
+    const token = jwt.sign(payload, config.jwtSecret);
+    return {
+      user,
+      token,
+    };
+  }
+
+  async sendRecovery(email) {
+
+    const user = await service.findEmail(email);
+    if (!user) {
+      throw (boom.unauthorized());
+    }
+    const payload = {
+      sub: user.id,
+    }
+    const token = jwt.sign(payload, config.jwtSecret, { expiresIn: '15min' });
+    const link = `http://localhost:3000/recovery/${token}`;
+
+    const mail = {
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: `${user.email}`, // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: `<b>Hello world? -> ${link}</b>`, // html body
+    }
+    const rta = await this.sendMail(mail);
+    return rta;
+  }
+
+  async sendMail(InfoEmail) {
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: 'testAccount.user',
+        pass: 'testAccount.pass',
+      },
+    });
+    await transporter.sendMail(
+      InfoEmail);
+    return { message: 'Email sent' };
+  }
+}
+
+module.exports = AuthService;
--- backend-Node_Passport-JWT/utils/auth/strategies/local.strategy.js
+++ backend-Node_Passport-JWT/utils/auth/strategies/local.strategy.js
@@ -1,10 +1,9 @@
 // local strategy passport
 
 const { Strategy } = require('passport-local');
-const boom = require('@hapi/boom');
-const bcrypt = require('bcrypt');
-const UserService = require('../../../services/user.service');
-const service = new UserService();
+
+const AuthService = require('../../../services/auth.service');
+const service = new AuthService();
 
 const LocalStrategy = new Strategy({
   usernameField: 'email',
@@ -12,16 +11,7 @@ const LocalStrategy = new Strategy({
 },
   async (email, password, done) => {
     try {
-      const user = await service.findEmail(email);
-      if (!user) {
-        done(boom.unauthorized(), false);
-      }
-      const isValid = await bcrypt.compare(password, user.password);
-      if (!isValid) {
-        done(boom.unauthorized(), false);
-      }
-      // user is returned by sequelize
-      delete user.dataValues.password;
+      const user = await service.findByEmail(email, password);
       done(null, user);
     } catch (error) {
       done(error, false);
