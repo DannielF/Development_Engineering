3022ac88f563803a79921df725e0cc455b1cffdb
DannielF <danielgranados1992@gmail.com>
2022-01-11 16:07:55 -0500

    [Add] tsconfig, main.ts fund_ts

---
+++ fundamentos_typeScript/src/hello.ts
@@ -0,0 +1,11 @@
+console.log('Hello TypeScript');
+
+/**
+ * cheat sheet
+ *
+ * tsc  -> compile all files
+ * tsc hello.ts
+ * tsc hello.ts --watch
+ * tsc --init (create tsconfig.json)
+ * tsc --project nameProject  -> run tsconfig.json in this project
+ */
